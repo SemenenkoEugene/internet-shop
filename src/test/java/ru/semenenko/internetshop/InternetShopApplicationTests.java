ce5127cf39dbc748f91dcdbbf68a335899ee95c6
SemenenkoEugene <gks-08@mail.ru>
2024-05-02 13:48:52 +0300

    Initial commit

---
+++ src/test/java/ru/semenenko/internetshop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.semenenko.internetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
