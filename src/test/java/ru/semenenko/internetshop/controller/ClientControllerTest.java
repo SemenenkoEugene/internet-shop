45978b7bf66da9802a0b6035f5a5b1de6736aadc
SemenenkoEugene <gks-08@mail.ru>
2024-05-06 17:32:07 +0300

    добавил тесты для проверки получение клиента по номеру телефона

--- src/test/java/ru/semenenko/internetshop/controller/ClientControllerTest.java
+++ src/test/java/ru/semenenko/internetshop/controller/ClientControllerTest.java
@@ -1,6 +1,5 @@
 package ru.semenenko.internetshop.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.semenenko.internetshop.dto.ClientDto;
 import ru.semenenko.internetshop.service.impl.ClientServiceImpl;
 
@@ -20,6 +17,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -41,18 +39,31 @@ class ClientControllerTest {
         when(clientService.createClient(any())).thenReturn(null);
 
         mockMvc.perform(post("/api/v1/clients")
-                .content(objectMapper.writeValueAsString(clientValidDto()))
-                .characterEncoding(StandardCharsets.UTF_8)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                        .content(objectMapper.writeValueAsString(clientValidDto()))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is2xxSuccessful());
 
-        verify(clientService,times(1)).createClient(any());
+        verify(clientService, times(1)).createClient(any());
+        verifyNoMoreInteractions(clientService);
+    }
+
+    @Test
+    public void getClientPhoneNumber_allValid() throws Exception {
+        when(clientService.getClientPhoneNumber(any(String.class))).thenReturn(clientValidDto());
+
+        mockMvc.perform(get("/api/v1/clients/{clientPhoneNumber}", "0123456789")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+
+        verify(clientService, times(1)).getClientPhoneNumber(any());
         verifyNoMoreInteractions(clientService);
     }
 
 
-    private ClientDto clientValidDto(){
+    private ClientDto clientValidDto() {
         return ClientDto.builder()
                 .firstname("Клиент")
                 .middlename("Клиентович")
--- src/test/java/ru/semenenko/internetshop/service/impl/ClientServiceImplTest.java
+++ src/test/java/ru/semenenko/internetshop/service/impl/ClientServiceImplTest.java
@@ -9,7 +9,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.semenenko.internetshop.dto.ClientDto;
 import ru.semenenko.internetshop.entity.Client;
 import ru.semenenko.internetshop.exception.ClientAlreadyExistsException;
+import ru.semenenko.internetshop.exception.ClientNotFoundException;
 import ru.semenenko.internetshop.repository.ClientRepository;
+
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -43,8 +46,6 @@ class ClientServiceImplTest {
         ClientDto clientDto = ClientDto.builder()
                 .phoneNumber("0123456789")
                 .build();
-        Client client = new Client();
-        client.setPhoneNumber("0123456789");
 
         when(clientRepository.save(any(Client.class)))
                 .thenThrow(new ClientAlreadyExistsException("Клиент с таким номером телефона уже существует"));
@@ -52,6 +53,39 @@ class ClientServiceImplTest {
                 Assertions.assertThrows(ClientAlreadyExistsException.class, () -> clientService.createClient(clientDto));
 
         Assertions.assertEquals("Клиент с таким номером телефона уже существует", exception.getMessage());
+    }
+
+    @Test
+    public void getClientPhoneNumber_whenIsOk() {
+
+        Client client = new Client();
+        String clientPhoneNumber = "0123456789";
+        client.setPhoneNumber(clientPhoneNumber);
+
+        when(clientRepository.findByPhoneNumber(clientPhoneNumber)).thenReturn(Optional.of(client));
+
+        clientService.getClientPhoneNumber(clientPhoneNumber);
+
+        verify(clientRepository, times(1)).findByPhoneNumber(clientPhoneNumber);
+        verifyNoMoreInteractions(clientRepository);
+    }
+
+    @Test
+    public void getClientPhoneNumber_when_ClientNotFound() {
+
+        String clientPhoneNumber = "0123456789";
+        ClientDto clientDto = ClientDto.builder()
+                .phoneNumber(clientPhoneNumber)
+                .build();
+
+        when(clientRepository.findByPhoneNumber(clientDto.getPhoneNumber()))
+                .thenThrow(new ClientNotFoundException("Клиент с таким номером телефона не найден"));
+
+        ClientNotFoundException exception =
+                Assertions.assertThrows(ClientNotFoundException.class,
+                        () -> clientService.getClientPhoneNumber(clientDto.getPhoneNumber()));
+
+        Assertions.assertEquals("Клиент с таким номером телефона не найден", exception.getMessage());
     }
 
 }
